c7074bcd95f76f58babb321c867a7de4d51c8060
Shana Russell <93401850+shanarussell@users.noreply.github.com>
2022-07-08 14:41:07 -0500

    changed colors, added information

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-console.log('Its working')
 
+//saves the current them in local storage
 let theme = localStorage.getItem('theme')
 
+//if there's no theme in local storage, set to light
 if(theme == null){
 	setTheme('light')
 }else{
 	setTheme(theme)
 }
 
+//dots under the photo
 let themeDots = document.getElementsByClassName('theme-dot')
 
 
+//iterate through the dots
+//listen for a click
+//set the mode based upon data-mode in html
+//set theme to the mode (call the function)
 for (var i=0; themeDots.length > i; i++){
 	themeDots[i].addEventListener('click', function(){
 		let mode = this.dataset.mode
@@ -19,6 +25,8 @@ for (var i=0; themeDots.length > i; i++){
 	})
 }
 
+//sets the theme based on what was clicked
+//puts current theme in local storage
 function setTheme(mode){
 	if(mode == 'light'){
 		document.getElementById('theme-style').href = 'default.css'
